716c342614f4635fa60cb0c58bc451fcb60b23ce
melliss67 <melliss67@yahoo.com>
2017-08-16 14:26:14 -0700

    Completed getting information from OMDBAPI

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
+import json
+
 import urllib.parse
+import urllib.request
 
 listing_page_html = """
 <!DOCTYPE html>
@@ -79,31 +82,40 @@ def get_key_value(movie, key):
         return 'N/A'
 
 
+def get_movie_info(url):
+    response = urllib.request.urlopen(url).read().decode('utf8')
+    obj = json.loads(response)
+    return obj
+    
+
+
 top_dir = input('top directory: ')
 print('searching for movies in "' + top_dir + '"')
 
 movie_names = get_movie_dirs(top_dir)
-print(movie_names)
 content = ''
 
 for cur_dir in movie_names:
-    # print(cur_dir)
     search = parse_movie_title(cur_dir)
-    # print(search)
     # will need to add api lookup here
     search_query = urllib.parse.urlencode(search)
-    api_url = "http://www.omdbapi.com/?apikey=7a6c480b&{}&plot=full".format(search_query)
-    print(api_url)
-    movie = {"Title":"Office Space","Year":"1999","Rated":"R","Released":"19 Feb 1999","Runtime":"89 min","Genre":"Comedy","Director":"Mike Judge","Writer":"Mike Judge (Milton animated shorts), Mike Judge (screenplay)","Actors":"Ron Livingston, Jennifer Aniston, David Herman, Ajay Naidu","Plot":"In the Initech office, the insecure Peter Gibbons hates his job and the abusive Division VP Bill Lumbergh that has just hired two consultants to downsize the company. His best friends are the software engineers Michael Bolton and Samir Nagheenanajar that also hate Initech, and his next door neighbor Lawrence. His girlfriend Anne is cheating on him but she convinces Peter to visit the hypnotherapist Dr. Swanson. Peter tells how miserable his life is and Dr. Swanson hypnotizes him and he goes into a state of ecstasy. However, Dr. Swanson dies immediately after giving the hypnotic suggestion to Peter. He dates the waitress Joanna and changes his attitude in the company, being promoted by the consultants. When he discovers that Michael and Samir will be fired, they decide to plant a virus in the account system to embezzle fraction of cents in each financial operation into Peter's account. However Michael commits a mistake in the software and instead of decimals, they steal a large amount. The desperate trio tries to fix the problem or they will go to prison.","Language":"English","Country":"USA","Awards":"2 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BOTA5MzQ3MzI1NV5BMl5BanBnXkFtZTgwNTcxNTYxMTE@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.8/10"},{"Source":"Rotten Tomatoes","Value":"79%"},{"Source":"Metacritic","Value":"68/100"}],"Metascore":"68","imdbRating":"7.8","imdbVotes":"210,817","imdbID":"tt0151804","Type":"movie","DVD":"31 Aug 1999","BoxOffice":"N/A","Production":"20th Century Fox","Website":"http://www.officeguy.com","Response":"True"}
-    year = get_key_value(movie, 'Year')
-    plot = get_key_value(movie, 'Plot')
-    movie_info = {'title': search['t'], 'year': year, 'plot': plot}
-    content = content + movie_html.format_map(movie_info)
+    api_url = "http://www.omdbapi.com/?apikey=7a6c480b&{}&plot=full".\
+            format(search_query)
+    # print(api_url)
+    movie = get_movie_info(api_url)
+    if get_key_value(movie,'Response') == 'False':
+        print('Movie titled {} not found.'.format(cur_dir))
+    else:
+        print('Adding {}.'.format(search['t']))
+        year = get_key_value(movie, 'Year')
+        plot = get_key_value(movie, 'Plot')
+        movie_info = {'title': search['t'], 'year': year, 'plot': plot}
+        content = content + movie_html.format_map(movie_info)
 
-    
-# print(content)
 result = listing_page_html.format(content)
 print(result)
+
+#OTHER INFORMATION
 
 # api key 7a6c480b
 
