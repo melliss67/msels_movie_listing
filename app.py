153daf5c36cb745e3d74d8d87559d45b97a8b894
melliss67 <melliss67@yahoo.com>
2017-08-18 09:55:36 -0700

    Added IMDB rating information and moved api key to a variable.

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import webbrowser
 import urllib.parse
 import urllib.request
 
+api_key = '7a6c480b'
+
 listing_page_html = """
 <!DOCTYPE html>
 <html>
@@ -40,6 +42,7 @@ listing_page_html = """
     border: 1px solid #003A21;
     padding: 4px;
     background-color: #FFFFFF;
+    white-space: nowrap;
   }}
   .small
   {{
@@ -78,10 +81,19 @@ listing_page_html = """
 
 
 movie_html = """
-<h1><a href="http://www.imdb.com/title/{imdbID}/" class="heading" target="_blank">{title}</a></h1>
-<div><p><a href="http://www.imdb.com/title/{imdbID}/" target="_blank"><img src="{poster}" alt="poster" width="100"></a>Year:{year}</p></div>
+<h1>
+  <a href="http://www.imdb.com/title/{imdbID}/" class="heading" target="_blank">{title}</a>
+</h1>
+<div>
+  <p>
+    <a href="http://www.imdb.com/title/{imdbID}/" target="_blank">
+    <img src="{poster}" alt="poster" width="100"></a>Year:{year}
+  </p>
+</div>
 <div>Runtime: {runtime}</div>
 <div>Metascore: {metascore}</div>
+<div>IMDB Rating: {imdbRating}</div>
+<div>IMDB Votes: {imdbVotes}</div>
 <div class="small"><p>Plot:{plot}</p></div>
 """
 
@@ -139,7 +151,7 @@ if len(movie_names) > 0:
     for cur_dir in movie_names:
         search = parse_movie_title(cur_dir)
         search_query = urllib.parse.urlencode(search)
-        api_url = "http://www.omdbapi.com/?apikey=7a6c480b&{}&plot=full".\
+        api_url = "http://www.omdbapi.com/?apikey=" + api_key + "&{}&plot=full".\
                 format(search_query)
 
         movie = get_movie_info(api_url)
@@ -153,9 +165,12 @@ if len(movie_names) > 0:
             plot = get_key_value(movie, 'Plot')
             poster = get_key_value(movie, 'Poster')
             imdbID = get_key_value(movie, 'imdbID')
+            imdbRating = get_key_value(movie, 'imdbRating')
+            imdbVotes = get_key_value(movie, 'imdbVotes')
             movie_info = {'title': search['t'], 'year': year, 'plot': plot,
                           'poster': poster, 'runtime': runtime,
-                          'metascore': metascore, 'imdbID': imdbID}
+                          'metascore': metascore, 'imdbID': imdbID,
+                          'imdbRating': imdbRating, 'imdbVotes': imdbVotes}
             content = content + movie_html.format_map(movie_info)
 
     result = listing_page_html.format(content)
@@ -166,13 +181,11 @@ if len(movie_names) > 0:
 
 #OTHER INFORMATION
 
-# api key 7a6c480b
-
 # sample output
 # {"Title":"Office Space","Year":"1999","Rated":"R","Released":"19 Feb 1999","Runtime":"89 min","Genre":"Comedy","Director":"Mike Judge","Writer":"Mike Judge (Milton animated shorts), Mike Judge (screenplay)","Actors":"Ron Livingston, Jennifer Aniston, David Herman, Ajay Naidu","Plot":"In the Initech office, the insecure Peter Gibbons hates his job and the abusive Division VP Bill Lumbergh that has just hired two consultants to downsize the company. His best friends are the software engineers Michael Bolton and Samir Nagheenanajar that also hate Initech, and his next door neighbor Lawrence. His girlfriend Anne is cheating on him but she convinces Peter to visit the hypnotherapist Dr. Swanson. Peter tells how miserable his life is and Dr. Swanson hypnotizes him and he goes into a state of ecstasy. However, Dr. Swanson dies immediately after giving the hypnotic suggestion to Peter. He dates the waitress Joanna and changes his attitude in the company, being promoted by the consultants. When he discovers that Michael and Samir will be fired, they decide to plant a virus in the account system to embezzle fraction of cents in each financial operation into Peter's account. However Michael commits a mistake in the software and instead of decimals, they steal a large amount. The desperate trio tries to fix the problem or they will go to prison.","Language":"English","Country":"USA","Awards":"2 nominations.","Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BOTA5MzQ3MzI1NV5BMl5BanBnXkFtZTgwNTcxNTYxMTE@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.8/10"},{"Source":"Rotten Tomatoes","Value":"79%"},{"Source":"Metacritic","Value":"68/100"}],"Metascore":"68","imdbRating":"7.8","imdbVotes":"210,817","imdbID":"tt0151804","Type":"movie","DVD":"31 Aug 1999","BoxOffice":"N/A","Production":"20th Century Fox","Website":"http://www.officeguy.com","Response":"True"}
 
 # sample url
-# http://www.omdbapi.com/?apikey=7a6c480b&t=office+space&y=1999&plot=full
+# http://www.omdbapi.com/?apikey=12345678&t=office+space&y=1999&plot=full
 
 
 
